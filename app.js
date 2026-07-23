83ef5c8ca1b7b19c505184fd2fcfcc21c72f9321
AbelGud <35884725+AbelGud@users.noreply.github.com>
2026-07-23 18:10:26 +0300

    insertion logic complited

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ console.log ("TEST")
 const express = require ("express");
 const mysql = require ("mysql2");
 
-
-
 //~step2. initiate express
 let app = express();
+
+//!Middleware()
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 
 
 //~step4. setup db connection
 let myDBconnection = mysql.createConnection({
     user:"crudpracticeuser",
-    password:'crudrracticeuser@1234',
+    password:'Abcd@123',
     host:'localhost',
     database:'crudPracticeDB',
 })
@@ -44,9 +46,62 @@ myDBconnection.connect((err) => {
 })
 
 //~step7. test API creation
-app.get('test',(req,res)=>{
+app.get('/test',(req,res)=>{
     res.send("backend is working!!");
 });
+
+
+//~step8. Creat table
+app.get ("/createTable",(req,res) => {
+    let tableSchema = `CREATE TABLE if not exists userInfo(
+    user_id int auto_increment,
+    user_first_name varchar(255) null,
+    user_last_name varchar(255) null,
+    user_email varchar(255) not null,
+    user_batch varchar(255) not null,
+    user_group varchar(255) not null,
+    user_course varchar(255) not null,
+    PRIMARY KEY(user_id))`;
+
+    myDBconnection.query(tableSchema,(err,data,field)=>{
+        //!err ; if there is any error while executing display
+       if(err) {
+        console.log(err);
+       } else {
+        res.send("Table created successfully !!");
+       }
+    });
+    
+});
+
+
+//~step9. data insertion
+app.post('/createUser',(req,res) => {
+
+    const {firstName, lastName, email, batch, group, course } = req.body;
+
+    let insertQuery = `
+    INSERT INTO userInfo(
+    user_first_name,
+    user_last_name,
+    user_email,
+    user_batch,
+    user_group,
+    user_course
+    )
+    VALUES (?,?,?,?,?,?)`;
+
+    myDBconnection.query(insertQuery,[firstName, lastName, email, batch, group, course], (err,data,field) => {
+    if(err) {
+        console.log(err)
+    } else {
+        res.send("data inserted successfully");
+    }
+       
+    },
+);
+});
+
 
 
 //~step3. creat server
